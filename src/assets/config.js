c767e5ee14c4061a3d07c326b3366099f2a2a808
Kenton Vizdos <kvizdos@gmail.com>
2018-08-14 23:51:53 +0000

    lasd

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,6 +1,6 @@
 export var config = {};
 
 config.urls = {
-    current: "https://api.therotg.com",
-    socket: "https://api.therotg.com/socket"
+    current: "https://api.bracketify.com",
+    socket: "https://api.bracketify.com/socket"
 }
