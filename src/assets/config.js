b5d4c79ceb1aade6987323c207385571a1e31a17
Kenton Vizdos (MainPC) <kvizdos@gmail.com>
2018-08-18 22:35:20 -0400

    API fix

--- src/assets/config.js
+++ src/assets/config.js
@@ -3,8 +3,8 @@ export var config = {};
 config.version = 1.1;
 
 config.urls = {
-    //current: "https://api.bracketify.com",
-    //socket: "https://api.bracketify.com/socket"
-    current: "http://localhost:3030",
-    socket: "http://localhost:3131"
+    current: "https://api.bracketify.com",
+    socket: "https://api.bracketify.com/socket"
+    //current: "http://localhost:3030",
+    //socket: "http://localhost:3131"
 }
